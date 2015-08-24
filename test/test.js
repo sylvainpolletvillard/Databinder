59c47efb503092b522764b1ceefd9a54b62b0080
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2015-08-24 23:01:10 +0200

    updated and simplified tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,31 @@
 (function(global){
 
+	var findAll = Element.prototype.findAll = function(selector){
+		return [].slice.call((this instanceof Element ? this : document).querySelectorAll(selector));
+	};
+
+	var find = Element.prototype.find = function(selector){
+		return (this instanceof Element ? this : document).querySelector(selector);
+	};
+
+	function fireClick(node){
+		if ( document.createEvent ) {
+			var evt = document.createEvent('MouseEvents');
+			evt.initEvent('click', true, false);
+			node.dispatchEvent(evt);
+		}
+	}
+
+	function fireChange(element) {
+		if ("fireEvent" in element)
+			element.fireEvent("onchange");
+		else {
+			var evt = document.createEvent("HTMLEvents");
+			evt.initEvent("change", false, true);
+			element.dispatchEvent(evt);
+		}
+	}
+
 	function detectGlobals(endFn){
 
 		function getIFrame(url, callback) {
@@ -47,7 +73,7 @@
 		});
 	});
 
-	var test1 = document.getElementById("test1");
+	var test1 = find("#test1");
 	databind(test1).set({
 		label:   "My website",
 		url:     "http://syllab.fr/",
@@ -60,25 +86,25 @@
 	});
 
 	test("binding attributes", function(){
-		var a = test1.querySelector('a');
+		var a = test1.find('a');
 		equal(a.textContent, "My website");
 		equal(a.href, "http://syllab.fr/");
 		equal(a.getAttribute("title"), "syllab.fr");
-		equal(test1.querySelector("p").innerHTML, "ok");
+		equal(test1.find("p").innerHTML, "ok");
 	});
 
-	var test2 = document.querySelector("#test2");
+	var test2 = find("#test2");
 
 	test("binding loop array", function(){
 
-		databind(test2.querySelector("ul")).set({
+		databind(test2).set({
 			emptylist: ["", undefined]
 		});
 
 		equal(test2.getElementsByTagName("ul").length, 1);
 		equal(test2.getElementsByTagName("li").length, 2);
 
-		databind(test2.querySelector("ul")).set({
+		databind(test2).set({
 			emptylist: []
 		});
 
@@ -86,9 +112,9 @@
 		equal(test2.getElementsByTagName("li").length, 0);
 
 		databind("table.tictactoe").set({
-			grid: [  ["X", " ", "O" ],
-						["O", "X", " " ],
-						[" ", " ", "X" ] ]
+			grid: [ ["X", " ", "O" ],
+					["O", "X", " " ],
+					[" ", " ", "X" ] ]
 		});
 
 		equal(test2.getElementsByTagName("tr").length, 3);
@@ -102,7 +128,7 @@
 		}).join(''),"X OOX   X");
 	});
 
-	var test3 = document.getElementById("palette");
+	var test3 = find("#palette");
 	databind("#palette").set({
 		palette: {
 			"Light Blue": "#ADD8E6",
@@ -118,19 +144,19 @@
 	});
 
 	test("traversing properties and indexes", function(){
-		equal(test3.querySelectorAll("ol li").length, 3);
-		equal(test3.querySelectorAll("ul li").length, 5);
-		equal(test3.querySelectorAll("ul li")[1].innerHTML, "Chocolate");
-		equal(test3.querySelectorAll("ol li b")[2].innerHTML, "2");
+		equal(test3.findAll("ol li").length, 3);
+		equal(test3.findAll("ul li").length, 5);
+		equal(test3.findAll("ul li")[1].innerHTML, "Chocolate");
+		equal(test3.findAll("ol li b")[2].innerHTML, "2");
 
 		var a = document.createElement('a');
 		a.style.color = "#808000";
 		a.style.backgroundColor = "#FA8072";
-		equal(test3.querySelectorAll("ul li")[2].style.color, a.style.color);
-		equal(test3.querySelectorAll("ol li")[2].style.backgroundColor, a.style.backgroundColor);
+		equal(test3.findAll("ul li")[2].style.color, a.style.color);
+		equal(test3.findAll("ol li")[2].style.backgroundColor, a.style.backgroundColor);
 	});
 
-	var test4 = document.getElementById("test4");
+	var test4 = find("#test4");
 	databind('#test4 input').set({
 		properties: {
 			blue: false,
@@ -150,18 +176,18 @@
 	});
 
 	test("class binding", function(){
-		equal(test4.querySelector("input").className, "red");
-		equal(test4.querySelector("button").className, "some classes as string");
-		var classes = test4.querySelector("section").className.split(' ').sort();
+		equal(test4.find("input").className, "red");
+		equal(test4.find("button").className, "some classes as string");
+		var classes = test4.find("section").className.split(' ').sort();
 		equal(classes.length, 3);
 		equal(classes.join(' '),"classes other some");
 
-		classes = test4.querySelector("p").className.split(' ').sort();
+		classes = test4.find("p").className.split(' ').sort();
 		equal(classes.length, 2);
 		equal(classes.join(' '), "important stuff");
 	});
 
-	var test5 = document.querySelector("#test5");
+	var test5 = find("#test5");
 
 	test("if binding", function(){
 		databind(test5).set({
@@ -174,9 +200,9 @@
 			lvl2: true,
 			lvl3: false
 		});
-		equal(test5.querySelectorAll("section").length, 0);
-		equal(test5.querySelectorAll("div").length, 0);
-		equal(test5.querySelectorAll("li").length, 3);
+		equal(test5.findAll("section").length, 0);
+		equal(test5.findAll("div").length, 0);
+		equal(test5.findAll("li").length, 3);
 		equal(test5.children.length, 2);
 
 		databind(test5).set({
@@ -189,13 +215,13 @@
 			lvl2: false,
 			lvl3: true
 		});
-		equal(test5.querySelectorAll("section").length, 1);
-		equal(test5.querySelectorAll("div").length, 1);
-		equal(test5.querySelectorAll("li").length, 5);
+		equal(test5.findAll("section").length, 1);
+		equal(test5.findAll("div").length, 1);
+		equal(test5.findAll("li").length, 5);
 		equal(test5.children.length, 4);
 	});
 
-	var test6 = document.querySelector("#test6");
+	var test6 = find("#test6");
 	databind(test6).set({
 		items: ["Some", "items", "seem", "to", "have", "disappeared"],
 		autocensor: function(){
@@ -204,7 +230,7 @@
 	});
 
 	test("visible binding", function(){
-		var oli = [].slice.call(test6.querySelectorAll("ol li"));
+		var oli = [].slice.call(test6.findAll("ol li"));
 		equal(oli.length, 6);
 		oli = oli.filter(function(li){
 			return li.style.display !== "none";
@@ -214,7 +240,7 @@
 			return li.innerHTML;
 		}).join(' '),"items to disappeared");
 
-		var uli = [].slice.call(test6.querySelectorAll("ul li"));
+		var uli = [].slice.call(test6.findAll("ul li"));
 		equal(uli.length, 6);
 		uli = uli.filter(function(li){
 			return li.style.display !== "none";
@@ -225,7 +251,7 @@
 		}).join(' '),"Some seem have");
 	});
 
-	var test7 = document.getElementById("test7");
+	var test7 = find("#test7");
 
 	test("functions and generators", function(){
 		var suite = [];
@@ -245,29 +271,29 @@
 			}
 		});
 
-		equal(test7.querySelectorAll("span").length, 11);
-		equal(test7.querySelectorAll("span.prime").length, 7);
-		equal([].map.call(test7.querySelectorAll("span"), function(i){
+		equal(test7.findAll("span").length, 11);
+		equal(test7.findAll("span.prime").length, 7);
+		equal([].map.call(test7.findAll("span"), function(i){
 			return i.innerHTML;
 		}).join('-'),"1-1-2-3-5-8-13-21-34-55-89");
-		equal([].map.call(test7.querySelectorAll("span.prime"), function(i){
+		equal([].map.call(test7.findAll("span.prime"), function(i){
 			return i.innerHTML;
 		}).join('-'),"1-1-2-3-5-13-89");
 	});
 
-	var test8 = document.getElementById("test8");
+	var test8 = find("#test8");
 
 	test("style binding", function(){
-		databind(test8.querySelector('input')).set({
+		databind(test8.find('input')).set({
 			otherRules: {
 				borderWidth: "10px",
 				borderColor: "red"
 			}
 		});
-		databind(test8.querySelector('button')).set({
+		databind(test8.find('button')).set({
 			ruleSet: "text-transform: uppercase; font-weight: bold;"
 		});
-		databind(test8.querySelector('p')).set({
+		databind(test8.find('p')).set({
 			message: "Big red stuff !",
 			color: "rgb(255,0,0)",
 			size: "3em"
@@ -278,19 +304,19 @@
 		testElem.style.borderColor = "red";
 		testElem.style.color = "rgb(255,0,0)";
 
-		equal(test8.querySelector("input").style.backgroundColor, testElem.style.backgroundColor);
-		equal(test8.querySelector("input").style.borderWidth, "10px");
-		equal(test8.querySelector("input").style.borderColor, testElem.style.borderColor);
-		equal(test8.querySelector("button").style.textTransform, "uppercase");
-		equal(test8.querySelector("button").style.fontWeight, "bold");
-		equal(test8.querySelector("button").style.fontWeight, "bold");
-		equal(test8.querySelector("p").innerHTML, "Big red stuff !");
-		equal(test8.querySelector("p").style.color, testElem.style.color);
-		equal(test8.querySelector("p").style.fontSize, "3em");
+		equal(test8.find("input").style.backgroundColor, testElem.style.backgroundColor);
+		equal(test8.find("input").style.borderWidth, "10px");
+		equal(test8.find("input").style.borderColor, testElem.style.borderColor);
+		equal(test8.find("button").style.textTransform, "uppercase");
+		equal(test8.find("button").style.fontWeight, "bold");
+		equal(test8.find("button").style.fontWeight, "bold");
+		equal(test8.find("p").innerHTML, "Big red stuff !");
+		equal(test8.find("p").style.color, testElem.style.color);
+		equal(test8.find("p").style.fontSize, "3em");
 
 	});
 
-	var test9 = document.getElementById("test9");
+	var test9 = find("#test9");
 	test("with binding", function(){
 
 		databind('#test9').set({
@@ -314,7 +340,7 @@
 		}
 	});
 
-	var test10 = document.getElementById("test10");
+	var test10 = find("#test10");
 	test("checked and selected bindings", function(){
 
 		databind(test10).set({
@@ -326,25 +352,16 @@
 			]
 		});
 
-		ok(test10.querySelector("input").checked === true);
+		ok(test10.find("input").checked === true);
 		equal(test10.getElementsByTagName("option").length, 3);
-		equal(test10.querySelector("select").value, "1");
-		equal(test10.querySelectorAll("option")[0].selected, false);
-		equal(test10.querySelectorAll("option")[1].selected, true);
-		equal(test10.querySelectorAll("option")[0].selected, false);
+		equal(test10.find("select").value, "1");
+		equal(test10.findAll("option")[0].selected, false);
+		equal(test10.findAll("option")[1].selected, true);
+		equal(test10.findAll("option")[0].selected, false);
 
 	});
 
-	var test11 = document.getElementById("test11");
-
-	function fireClick(node){
-		if ( document.createEvent ) {
-			var evt = document.createEvent('MouseEvents');
-			evt.initEvent('click', true, false);
-			node.dispatchEvent(evt);
-		}
-	}
-
+	var test11 = find("#test11");
 	test("events binding", function(){
 
 		var List = function(selector){
@@ -377,25 +394,25 @@
 		myList.addItem();
 		myList.databinding.reset();
 
-		equal(test11.querySelectorAll("li").length, 3);
+		equal(test11.findAll("li").length, 3);
 
-		fireClick(test11.querySelectorAll("li a")[1]);
+		fireClick(test11.findAll("li a")[1]);
 
-		equal(test11.querySelectorAll("li").length, 2);
+		equal(test11.findAll("li").length, 2);
 		equal(myList.itemCollection.length, 2);
 		equal(myList.itemCollection[0].num, 1);
 		equal(myList.itemCollection[1].num, 3);
 
-		fireClick(test11.querySelector("input"));
-		fireClick(test11.querySelector("input"));
+		fireClick(test11.find("input"));
+		fireClick(test11.find("input"));
 
-		equal(test11.querySelectorAll("li").length, 4);
+		equal(test11.findAll("li").length, 4);
 		equal(myList.itemCollection.length, 4);
 		equal(myList.itemCollection[2].num, 4);
 
 	});
 
-	var test12 = document.getElementById("test12");
+	var test12 = find("#test12");
 
 	function fireMouseEvent(node, type){
 		if ( document.createEvent ) {
@@ -423,7 +440,7 @@
 				this.currentRect = null;
 				var event = document.createEvent("CustomEvent");
 				event.initCustomEvent('rectangle', false, false, null);
-				test12.querySelector("svg").dispatchEvent(event);
+				test12.find("svg").dispatchEvent(event);
 			},
 			update: function(e, scope, elm) {
 				if(scope.currentRect != null){
@@ -436,14 +453,14 @@
 			}
 		});
 
-		var svg = test12.querySelector("svg");
+		var svg = test12.find("svg");
 		fireMouseEvent(svg, "mousedown");
 		fireMouseEvent(svg, "mousemove");
 		fireMouseEvent(svg, "mouseup");
 		equal(rectanglesCounter, 1);
 	});
 
-	var test13 = document.getElementById("test13");
+	var test13 = find("#test13");
 	test("complex bindings value paths", function(){
 
 		databind(test13).set({
@@ -464,23 +481,23 @@
 
 	});
 
-	var test14 = document.getElementById("test14");
+	var test14 = find("#test14");
 	test("text and html bindings", function(){
 		databind(test14).set({
 			msg: "try to <strong>inject</strong> <iframe></iframe> html <span>stuff</span>"
 		});
-		equal(test14.querySelector(".text").children.length, 0);
-		equal(test14.querySelector(".text iframe"), null);
-		equal(test14.querySelector(".text span"), null);
-		equal(test14.querySelector(".html").children.length, 3);
-		notEqual(test14.querySelector(".html iframe"), null);
-		notEqual(test14.querySelector(".html span"), null);
-		ok([].every.call(test14.querySelectorAll(".undefined"), function(elm){
+		equal(test14.find(".text").children.length, 0);
+		equal(test14.find(".text iframe"), null);
+		equal(test14.find(".text span"), null);
+		equal(test14.find(".html").children.length, 3);
+		notEqual(test14.find(".html iframe"), null);
+		notEqual(test14.find(".html span"), null);
+		ok([].every.call(test14.findAll(".undefined"), function(elm){
 			return elm.innerHTML === "do not touch";
 		}));
 	});
 
-	var test15 = document.getElementById("test15");
+	var test15 = find("#test15");
 	test("template binding", function(){
 		databind(test15).set({
 			africa: [
@@ -500,11 +517,11 @@
 			]
 		});
 
-		equal(test15.querySelectorAll("#flags img").length, 9);
-		equal(test15.querySelectorAll("#flags h3").length, 9);
-		equal(test15.querySelectorAll("#flags dd").length, 9);
-		equal(test15.querySelectorAll("#flags h2")[2].innerHTML, "Africa");
-		equal([].map.call(test15.querySelectorAll("#flags h3"), function(country){
+		equal(test15.findAll("#flags img").length, 9);
+		equal(test15.findAll("#flags h3").length, 9);
+		equal(test15.findAll("#flags dd").length, 9);
+		equal(test15.findAll("#flags h2")[2].innerHTML, "Africa");
+		equal([].map.call(test15.findAll("#flags h3"), function(country){
 			return country.innerHTML;
 		}).join(","), "France,Sweden,Germany,Russia,Israel,Japan,Senegal,Namibia,Egypt");
 
@@ -566,7 +583,7 @@
 		return new Date(this).getFullYear();
 	};
 
-	var test16 = document.getElementById("test16");
+	var test16 = find("#test16");
 
 	test("extensions", function(){
 		databind(test16).set({
@@ -574,11 +591,11 @@
 			num: 1234.567,
 			date: 2012.345
 		});
-		equal(test16.querySelector("p").innerHTML, "1235");
-		equal(test16.querySelector("span").innerHTML, "2012");
+		equal(test16.find("p").innerHTML, "1235");
+		equal(test16.find("span").innerHTML, "2012");
 	});
 
-	var test17 = document.getElementById("test17");
+	var test17 = find("#test17");
 	test("automatic bindings", function(){
 		var span1 = document.createElement("span");
 		var span2 = document.createElement("span");
@@ -599,29 +616,27 @@
 				helloworld: "Hello world !"
 			}
 		});
-		equal(test17.querySelectorAll("li").length, 3);
-		equal(test17.querySelectorAll("span").length, 3);
-		equal(test17.querySelectorAll("span")[2].innerHTML, "3");
-		equal(test17.querySelectorAll("option").length, 3);
-		ok(test17.querySelectorAll("option")[2] && test17.querySelectorAll("option")[2].selected);
-		ok(test17.querySelector("[name='truthy']").checked);
-		equal(test17.querySelector("#falsy"), null);
-		equal(test17.querySelector("figure"), null);
-		equal(test17.querySelector("img").getAttribute("src"), "../site/res/flags/fr.png");
-		equal(test17.querySelector("input[name='helloworld']").value, "Hello world !");
-		equal(test17.querySelector("p").textContent, "Hello world !");
+		equal(test17.findAll("li").length, 3);
+		equal(test17.findAll("span").length, 3);
+		equal(test17.findAll("span")[2].innerHTML, "3");
+		equal(test17.findAll("option").length, 3);
+		ok(test17.findAll("option")[2] && test17.findAll("option")[2].selected);
+		ok(test17.find("[name='truthy']").checked);
+		equal(test17.find("#falsy"), null);
+		equal(test17.find("figure"), null);
+		equal(test17.find("img").getAttribute("src"), "../site/res/flags/fr.png");
+		equal(test17.find("input[name='helloworld']").value, "Hello world !");
+		equal(test17.find("p").textContent, "Hello world !");
 	});
 
-
-	/*var test18 = document.getElementById("test18");
-	test("view saves with get", function() {
+	var test18 = find("#test18");
+	test("view input binding and auto updates", function() {
 
 		function save(){
-			databind(test18.querySelector("form")).get();
-			databind(test18.querySelector("p")).reset();
+			databind(test18.find("p")).reset();
 		}
 
-		databind(test18).set({
+		var data = {
 			scores: [
 				{ name: "Joe", score: 6500 },
 				{ name: "Jack", score: 8200 },
@@ -636,24 +651,29 @@
 				event.preventDefault();
 				save();
 			}
-		});
+		};
+		databind(test18).set(data);
 
-		equal(test18.querySelectorAll("span")[0].textContent, "Jack");
-		equal(test18.querySelectorAll("span")[1].textContent, "8200");
-		test18.querySelectorAll("input")[2].value = "Jeff";
+		equal(test18.findAll("span")[0].textContent, "Jack");
+		equal(test18.findAll("span")[1].textContent, "8200");
+		test18.findAll("input")[2].value = "Jeff";
+		fireChange(test18.findAll("input")[2]);
+		equal(data.scores[1].name, "Jeff");
 		save();
-		equal(test18.querySelectorAll("span")[0].textContent, "Jeff");
-		test18.querySelectorAll("input")[3].value = 5000;
+		equal(test18.findAll("span")[0].textContent, "Jeff");
+		test18.findAll("input")[3].value = 5000;
+		fireChange(test18.findAll("input")[3]);
+		equal(data.scores[1].score, 5000);
 		save();
-		equal(test18.querySelectorAll("span")[0].textContent, "Joe");
-		equal(test18.querySelectorAll("span")[1].textContent, "6500");
-	});*/
+		equal(test18.findAll("span")[0].textContent, "Joe");
+		equal(test18.findAll("span")[1].textContent, "6500");
+	});
 
-	var test19 = document.getElementById("test19");
+	var test19 = find("#test19");
 	test("string and number parameters", function() {
 		databind(test19).set();
-		equal(test19.querySelectorAll("span").length, 4);
-		equal(test19.querySelector("p").innerHTML, "test string parameter");
+		equal(test19.findAll("span").length, 4);
+		equal(test19.find("p").innerHTML, "test string parameter");
 	});
 
 })(this);
